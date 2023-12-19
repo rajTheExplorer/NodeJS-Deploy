ab5b3a27723d28f1118362c51adcba1c0a64288a
Muni Raj <rajmuni.v@gmail.com>
2023-12-19 23:34:09 +0530

    bill

--- Backend/routes/Google.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import HandleGoogleApi from '../controllers/HandleGoogleApi.js'
-let router=express.Router()
-
-
-router.post('/google',HandleGoogleApi)
-
-export default router
--- Backend/routes/TollGuru.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import HandleTollGuruApi from "../controllers/HandleTollGuruApi.js";
-let router=express.Router()
-
-
-router.post('/tollguru',HandleTollGuruApi)
-
-
-export default router
